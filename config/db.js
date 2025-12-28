4614838c23ac906c0a7363173542d1d3799264e8
hasina1998-svg <hasinadhungel1998@gmail.com>
2025-12-28 21:58:52 +0545

    fixes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
+  
+  try {   
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
