56356dc9fb34857dff247e15ed4d5b3e12e44f19
hasina1998-svg <hasinadhungel1998@gmail.com>
2026-03-21 18:25:20 +0545

    blog issue fixes

--- tmp_check_indexes.js
+++ tmp_check_indexes.js
@@ -7,7 +7,7 @@ import Blog from "./models/blog.model.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const checkIndexes = async () => {
     try {
